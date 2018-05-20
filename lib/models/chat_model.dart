b0d35c97933bab07aa7a1aa8328d837bfbe46de3
kmachoLaptop <Cailo2009>
2018-05-20 11:19:31 -0400

    Adding new files.

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,58 @@
+class ChatModel{
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name,this.message,this.time,this.avatarUrl});
+}
+  List<ChatModel> dummyData =[
+    new ChatModel(
+      name: "Juan Camacho",
+      message: "Flutter es lo mejor!!",
+      time: "5:10",
+      avatarUrl: "https://pre00.deviantart.net/ef23/th/pre/i/2017/294/2/9/goku_miggate_no_goku_i_3_584_by_koku78-dbrb1j0.png"
+    ),
+    new ChatModel(
+      name: "Fernan2",
+      message: "kmacho metiendo mano, el tipo es duro!!",
+      time: "15:11",
+      avatarUrl: "https://vignette.wikia.nocookie.net/dragonball/images/0/01/KrillinBU01.png/revision/latest?cb=20100515144143"
+    ),
+    new ChatModel(
+      name: "Ramon USA",
+      message: "Estoy terminando el Facebook en Flutter",
+      time: "15:13",
+      avatarUrl: "http://img.morphthing.com/i/27341/2/0/76bac43b/Shrek.jpeg"
+    ),
+    new ChatModel(
+      name: "Nelson .Net",
+      message: "Lo hare en xamarin, aunque tenga pila de bugs..",
+      time: "15:15",
+      avatarUrl: "http://2u4p9f41q51y4bu6tr1bnbhr.wpengine.netdna-cdn.com/wp-content/uploads/2017/04/Mr.Popo_.png"
+    ),
+    new ChatModel(
+      name: "Gaby H",
+      message: "Ahora, me haré un tatuaje de Dart",
+      time: "15:16",
+      avatarUrl: "https://images-na.ssl-images-amazon.com/images/I/71M1-gNkhnL._SY550_.jpg"
+    ),
+    new ChatModel(
+      name: "Francel",
+      message: "Lo haré en un momento",
+      time: "15:17",
+      avatarUrl: "http://static.alfabetajuega.com/abj_public_files/multimedia/imagenes/201706/196758.alfabetajuega-yamcha.jpg"
+    ),
+    new ChatModel(
+      name: "Juan C",
+      message: "Dejame desinstalar eclipse.",
+      time: "15:20",
+      avatarUrl: "https://vignette.wikia.nocookie.net/dragonball/images/c/cf/Picollo_Portrait.jpg/revision/latest?cb=20100813075704"
+    ),
+     new ChatModel(
+      name: "Jordany Ahorro y Prestamos S.A.",
+      message: "interesante",
+      time: "15:21",
+      avatarUrl: "https://vignette.wikia.nocookie.net/bobesponja/images/0/0b/Bob_Esponja_Don_Cangrejo.png/revision/latest?cb=20170412005839"
+    )
+  ];
